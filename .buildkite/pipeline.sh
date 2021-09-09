056b481a0903e3d158719c33cd841de94faae562
Amir Zarrinkafsh <nightah@me.com>
2021-09-09 20:18:21 +1000

    ci(buildkite): add buildkite

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+REPOSITORY="authelia/buildkite"
+
+if [[ ${BUILDKITE_TAG} == "" ]]; then
+  TAG="latest"
+else
+  TAG=${BUILDKITE_TAG}
+fi
+
+cat << EOF
+steps:
+  - label: ":docker: Build and Deploy"
+    commands:
+      - "docker build --tag ${REPOSITORY}:${TAG} --no-cache=true --pull=true ."
+      - "docker push ${REPOSITORY}:${TAG}"
+    agents:
+      upload: "fast"
+
+  - wait:
+    if: build.branch == "master"
+
+  - command: "curl \"https://ci.nerv.com.au/readmesync/update?github_repo=${REPOSITORY}&dockerhub_repo=${REPOSITORY}\""
+    label: ":docker: Update README.md"
+    if: build.branch == "master"
+EOF
