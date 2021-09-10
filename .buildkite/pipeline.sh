0d0d9633dc077d6bf8ad3bab8e203b39ec036f00
Amir Zarrinkafsh <nightah@me.com>
2021-09-10 14:19:58 +1000

    ci(buildkite): add concurrency group for deploy step

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -15,14 +15,25 @@ steps:
     commands:
       - "docker build --tag ${REPOSITORY}:${TAG} --no-cache=true --pull=true ."
       - "docker push ${REPOSITORY}:${TAG}"
+    concurrency: 1
+    concurrency_group: "deployments"
     agents:
       upload: "fast"
+    if: build.branch == "master"
+
+  - label: ":docker: Build and Deploy"
+    commands:
+      - "docker build --tag ${REPOSITORY}:${TAG} --no-cache=true --pull=true ."
+      - "docker push ${REPOSITORY}:${TAG}"
+    agents:
+      upload: "fast"
+    if: build.branch != "master"
 
   - wait:
     if: build.branch == "master"
 
-  - command: "curl \"https://ci.nerv.com.au/readmesync/update?github_repo=${REPOSITORY}&dockerhub_repo=${REPOSITORY}\""
-    label: ":docker: Update README.md"
+  - label: ":docker: Update README.md"
+    command: "curl \"https://ci.nerv.com.au/readmesync/update?github_repo=${REPOSITORY}&dockerhub_repo=${REPOSITORY}\""
     if: build.branch == "master"
     agents:
       upload: "fast"
